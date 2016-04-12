7f6e0213d7af152987c578f515bdb532cdc72f18
Josh <you@example.coexit>
2016-04-11 22:51:28 -0500

    render nav content

--- app/scripts/entry.js
+++ app/scripts/entry.js
@@ -3,7 +3,7 @@ import './../styles/main.scss';
 import cat from './../assets/orangecat.jpg';
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import logo from './../assets/logo.png';
 // import a module from another file.
 import tiy from './app.js';
 
@@ -11,43 +11,50 @@ import tiy from './app.js';
 tiy();
 
 
-const Nav = React.createClass({
-	render: function(){
-		return <nav><a>This is the nav</a></nav>
-	}
-});
+ 
 
-const ListItem = React.createClass({
-    render: function() {
-        return     <li> 
-                <img src={cat} />
-                <p>this is a list item </p>
-                </li>;
-    }
-});
 
-const List = React.createClass({
-    render: function() {
-    	let myListItems = [
-       			<ListItem key="1" />,
-       			<ListItem key="2" />,
-       			<ListItem key="3" />,
-       			<ListItem key="4" />,
-       			<ListItem key="5" />
-               ]; 
-                 return (
-            <ul>
-            	{myListItems}     
-            </ul>
-        );
-    }
-})
 
-const NavBar = React.createClass({
+const NavComponent = React.createClass({
 	render: function() {
-		return <nav><a href="#">this is a link</a></nav>;
+		return (
+			<nav>
+				<div className="navContent">
+						<img src={logo} className="logo-image" />
+						<h2 className="logo-text">Fresh Jobs</h2>
+				</div>
+				<div class="links"> 
+						<a href="#">JOBS</a>
+						<a href="#">COMPANIES</a>
+						<a href="#">CITIES</a>
+						<a href="#">WHY FRESH?</a>
+						<a href="#">FOR EMPLOYERS</a>
+				</div>
+			</nav>
+		);
 	}
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -58,7 +65,7 @@ const TestComponent = React.createClass({
 });
 
 
-
+ReactDOM.render(<NavComponent/>, document.querySelector('div'));
 ReactDOM.render(<TestComponent/>, document.querySelector('h1'));
 ReactDOM.render(<NavBar/>, document.querySelector('nav'));
 
