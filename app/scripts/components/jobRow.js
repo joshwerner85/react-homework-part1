77222574bc5c18fe1864bad997d3811abbc17d1c
Josh <you@example.coexit>
2016-04-12 21:28:39 -0500

    job-row rendering YES

--- app/scripts/components/jobRow.js
+++ app/scripts/components/jobRow.js
@@ -5,20 +5,23 @@ import React from 'react';
 const JobComponent = React.createClass({
 	render: function() {
 		return (
-			<div className="container">
-			<form id="login">
-				<label className="keywords">Keywords</label>
-				<input className="keywords-input"/>
-
-				<label className="location">Location</label>
-				<input className="location-input"/>
-				<button>Search</button>
-			</form>
-			</div>
+			 
+		 <div className="job-row">
+		    <h1 className="seniorengineer">Senior Frontend Engineer</h1>
+		    <p className="yesterday">yesterday</p>
+		    <h4 className="nsone">NSONE - <span>New York,NY</span></h4>
+		    <p className="description">Senior Frontend EngineerSolve Hard Problems With a Team of Infastructure ProsNSONE is based in New York...</p>
+		    <div className="angularjs">Angularjs</div>
+		    <div className="d3js">d3.js</div>
+		    <div className="javascript">javascript</div>
+		    <div className="ui">ui</div>
+		    <div className="css">css</div>
+		</div>
 		);
 	}
 });
 
 
 export default JobComponent;
+ 
  
